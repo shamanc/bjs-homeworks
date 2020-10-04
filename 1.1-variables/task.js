d055a08f88bd66570c906c74fb76711288ee756f
Shaman <im.gornostaev@gmail.com>
2020-10-05 00:18:37 +0400

    the very first commit variables

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -8,20 +8,37 @@
 function averageMark(a, g, p) {
 	
     // реализуйте здесь задание №1
+
+    let algebra = a;
+    let geography = g;
+    let physics = p;
+
+    let avg = (algebra + geography + physics) / 3;
     
-	return // ваш ответ;
+	return avg;
 }
 
 function sayHello(userName) {
 	
     // реализуйте здесь задание №2
+
+    let myName = userName;
+    let message = `Привет, мир! Меня зовут ${myName}`;
     
-	return // ваш ответ;
+	return message;
+
+
 }
 
 function calculateFormula() {
     
 	// реализуйте здесь задание №3
+
+	let x = 2;
+	let y = 22;
+	let z = 0;
+
+	let result = x * y + 5 * z + x - 1;
     
-    return // ваш ответ;
+    return result;
 }
